6b9f26291cee66b959fc5dca84d715fd0a8e31a0
unknown <feikomilcezzz2@gmail.com>
2024-10-03 19:00:24 +0300

    feat: zip tasks done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import fs from 'node:fs';
+import path from 'node:path';
+
 const compress = async () => {
-    // Write your code here 
+  const dir = path.join(import.meta.dirname, 'files');
+  const fileToCompress = path.join(dir, 'fileToCompress.txt');
+  const fileToWrite = path.join(dir, 'archive.gz');
+
+  const readStream = fs.createReadStream(fileToCompress);
+
+  const writeStreamDestination = fs.createWriteStream(fileToWrite);
+
+  const gzip = createGzip();
+
+  await pipeline(readStream, gzip, writeStreamDestination);
 };
 
 await compress();
