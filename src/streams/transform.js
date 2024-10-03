528dd8b879353295fc989104f50ac6c45dc22371
unknown <feikomilcezzz2@gmail.com>
2024-10-03 14:44:49 +0300

    fix: transform \n to os.EOL

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,10 +1,14 @@
 import { Transform } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
+import os from 'node:os';
 
 const transform = async () => {
   const transformStream = new Transform({
     transform(chunk, encoding, callback) {
-      callback(null, `${chunk.toString().split('').reverse().join('')}\n`);
+      callback(
+        null,
+        `${chunk.toString().split('').reverse().join('')}${os.EOL}`
+      );
     },
   });
 
