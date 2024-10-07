78fbbbfa37969da87b227c4b9b6fc93c637def3b
unknown <feikomilcezzz2@gmail.com>
2024-10-07 14:02:17 +0300

    feat: cp task done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import path from 'node:path';
+import { fork } from 'node:child_process';
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcessFile = path.join(import.meta.dirname, 'files', 'script.js');
+
+  const childProcess = fork(childProcessFile, args, { silent: true });
+
+  process.stdin.on('data', (data) => {
+    childProcess.stdin.write(data);
+  });
+
+  childProcess.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  childProcess.on('exit', () => {
+    process.exit();
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hello', 'world']);
